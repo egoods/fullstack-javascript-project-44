d29a0ae30ecc8d9ff985a34aff7673d440e8abc1
Egoods <forclearlife@gmail.com>
2022-10-16 22:24:49 +0300

    Create greetings function

---
+++ bin/cli.js
@@ -0,0 +1,7 @@
+import readlineSync from 'readline-sync';
+
+export const SayHello = () => {
+    console.log("Welcome to the Brain Games!");
+    const name = readlineSync.question('May i have your name? ');
+    console.log(`Hello, ${name}!`);
+};
