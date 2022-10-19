0c2bb8291f895d4294275e87b416944fdebe97e8
Egoods <forclearlife@gmail.com>
2022-10-20 01:13:46 +0300

    Fix calc game

--- bin/games/calc-game.js
+++ bin/games/calc-game.js
@@ -2,19 +2,19 @@ import launchGame from './game-engine.js';
 
 const printRules = () => console.log('What is the result of the expression?');
 
-const operationsList = {
-  '+': (a, b) => a + b,
-  '-': (a, b) => a - b,
-  '*': (a, b) => a * b,
-};
-
-const getRandomOperation = () => {
-  const keys = Object.keys(operationsList);
-
-  return keys[Math.round(Math.random() * keys.length)];
-};
-
 const getQuestionAndAnswer = () => {
+  const operationsList = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+  };
+
+  const getRandomOperation = () => {
+    const keys = Object.keys(operationsList);
+
+    return keys[Math.round(Math.random() * keys.length)];
+  };
+
   const fisrtOperand = Math.round(Math.random() * 100);
   const secondOperand = Math.round(Math.random() * 100);
   const operation = getRandomOperation();
